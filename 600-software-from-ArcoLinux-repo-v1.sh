5518eece393df754903613afe8f6e4d4f3821da6
j. kaiden <jakekaiden@gmail.com>
2020-03-12 17:58:05 +0100

    Update 600-software-from-ArcoLinux-repo-v1.sh

--- 600-software-from-ArcoLinux-repo-v1.sh
+++ 600-software-from-ArcoLinux-repo-v1.sh
@@ -20,7 +20,7 @@ echo "####                ARCOLINUX FOLDER                        ####"
 echo "################################################################"
 echo
 
-sudo pacman -S arcolinux-arc-themes-nico-git --noconfirm --needed
+#sudo pacman -S arcolinux-arc-themes-nico-git --noconfirm --needed
 sudo pacman -S arcolinux-bin-git --noconfirm --needed
 #sudo pacman -S arcolinux-common-git --noconfirm --needed
 sudo pacman -S arcolinux-conky-collection-git --noconfirm --needed
@@ -29,9 +29,9 @@ sudo pacman -S arcolinux-cron-git --noconfirm --needed
 #sudo pacman -S arcolinux-docs-git --noconfirm --needed
 #sudo pacman -S arcolinux-faces-git --noconfirm --needed
 sudo pacman -S arcolinux-fonts-git --noconfirm --needed
-sudo pacman -S arcolinux-geany-git --noconfirm --needed
+#sudo pacman -S arcolinux-geany-git --noconfirm --needed
 sudo pacman -S arcolinux-hblock-git --noconfirm --needed
-sudo pacman -S arcolinux-kvantum-git --noconfirm --needed
+#sudo pacman -S arcolinux-kvantum-git --noconfirm --needed
 #sudo pacman -S arcolinux-kvantum-lxqt-git --noconfirm --needed
 #sudo pacman -S arcolinux-kvantum-plasma-git --noconfirm --needed
 sudo pacman -S arcolinux-lightdm-gtk-greeter --noconfirm --needed
@@ -52,8 +52,8 @@ sudo pacman -S arcolinux-nitrogen-git --noconfirm --needed
 #sudo pacman -S arcolinux-obmenu-generator-xtended-git --noconfirm --needed
 #sudo pacman -S arcolinux-openbox-themes-git --noconfirm --needed
 sudo pacman -S arcolinux-pipemenus-git --noconfirm --needed
-sudo pacman -S arcolinux-plank-git --noconfirm --needed
-sudo pacman -S arcolinux-plank-themes-git --noconfirm --needed
+#sudo pacman -S arcolinux-plank-git --noconfirm --needed
+#sudo pacman -S arcolinux-plank-themes-git --noconfirm --needed
 #sudo pacman -S arcolinux-polybar-git --noconfirm --needed
 sudo pacman -S arcolinux-qt5-git --noconfirm --needed
 #sudo pacman -S arcolinux-qt5-plasma-git --noconfirm --needed
@@ -63,11 +63,11 @@ sudo pacman -S arcolinux-root-git --noconfirm --needed
 #sudo pacman -S arcolinux-slim --noconfirm --needed
 #sudo pacman -S arcolinux-slimlock-themes-git --noconfirm --needed
 sudo pacman -S arcolinux-system-config-git --noconfirm --needed
-sudo pacman -S arcolinux-termite-themes-git --noconfirm --needed
+#sudo pacman -S arcolinux-termite-themes-git --noconfirm --needed
 #sudo pacman -S arcolinux-tint2-git --noconfirm --needed
 #sudo pacman -S arcolinux-tint2-themes-git --noconfirm --needed
 sudo pacman -S arcolinux-tweak-tool-git --noconfirm --needed
-sudo pacman -S arcolinux-variety-git --noconfirm --needed
+#sudo pacman -S arcolinux-variety-git --noconfirm --needed
 sudo pacman -S arcolinux-wallpapers-git --noconfirm --needed
 sudo pacman -S arcolinux-welcome-app-git --noconfirm --needed
 #sudo pacman -S arcolinux-wallpapers-lxqt-dual-git --noconfirm --needed
