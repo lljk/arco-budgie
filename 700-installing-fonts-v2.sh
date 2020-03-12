130318c6032f3b47727bb2b16f0b1750465d2419
j. kaiden <jakekaiden@gmail.com>
2020-03-12 17:59:27 +0100

    Update 700-installing-fonts-v2.sh

--- 700-installing-fonts-v2.sh
+++ 700-installing-fonts-v2.sh
@@ -17,18 +17,18 @@ set -e
 
 echo "Installing fonts from Arch Linux repo"
 
-sudo pacman -S adobe-source-sans-pro-fonts --noconfirm --needed
+#sudo pacman -S adobe-source-sans-pro-fonts --noconfirm --needed
 sudo pacman -S cantarell-fonts --noconfirm --needed
 sudo pacman -S noto-fonts --noconfirm --needed
 sudo pacman -S ttf-bitstream-vera --noconfirm --needed
-sudo pacman -S ttf-dejavu --noconfirm --needed
+#sudo pacman -S ttf-dejavu --noconfirm --needed
 sudo pacman -S ttf-droid --noconfirm --needed
 sudo pacman -S ttf-hack --noconfirm --needed
-sudo pacman -S ttf-inconsolata --noconfirm --needed
-sudo pacman -S ttf-liberation --noconfirm --needed
+#sudo pacman -S ttf-inconsolata --noconfirm --needed
+#sudo pacman -S ttf-liberation --noconfirm --needed
 sudo pacman -S ttf-roboto --noconfirm --needed
 sudo pacman -S ttf-ubuntu-font-family --noconfirm --needed
-sudo pacman -S tamsyn-font --noconfirm --needed
+#sudo pacman -S tamsyn-font --noconfirm --needed
 
 echo "################################################################"
 echo "####        Fonts from Arch Linux repo have been installed        ####"
